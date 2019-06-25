fa0e187e304b4d587413d8deb84fdd53bc09e40a
Melinna Rojas Camargo <melin1504@gmail.com>
2019-06-25 18:05:21 -0500

    graph

--- button.js
+++ button.js
@@ -1,13 +1,23 @@
-$(document).on('change','.btn-file :file',function(){
-    var input = $(this);
-    var numFiles = input.get(0).files ? input.get(0).files.length : 1;
-    var label = input.val().replace(/\\/g,'/').replace(/.*\//,'');
-    input.trigger('fileselect',[numFiles,label]);
-  });
-  $(document).ready(function(){
-    $('.btn-file :file').on('fileselect',function(event,numFiles,label){
-      var input = $(this).parents('.input-group').find(':text');
-      var log = numFiles > 1 ? numFiles + ' files selected' : label;
-      if(input.length){ input.val(log); }else{ if (log) alert(log); }
-    });
-  });
+$('#f').on('change', function (ev) {
+  var canvas = document.getElementById("micanvas");
+  var ctx = canvas.getContext("2d");
+  var img = new Image();
+
+
+  var f = ev.target.files[0];
+  console.log(f);
+
+  img.src = "img/" + f.name;
+  img.style = "max-width:100%;";
+  img.onload = function () {
+    ctx.drawImage(img, 0, 0);
+  }
+  var fr = new FileReader();
+  console.log(fr);
+  fr.onload = function (ev2) {
+    console.dir(ev2);
+    $('#i').attr('src', ev2.target.result);
+  };
+  fr.readAsDataURL(f);
+
+});
--- graph.ts
+++ graph.ts
@@ -1,37 +1,155 @@
 class Nodo {
-  private item: number;
-  private next: Nodo;
+  public item: Pixel;
+  public vertex: number;
+  public next: Nodo;
+}
+
+class Pixel {
+  public r: number;
+  public g: number;
+  public b: number;
+  public a: number;
 }
 
 //TODO : Implement Iterator for Bags
-class Bag {
+class MyBag {
   private n: number;
   private primero: Nodo;
 
   constructor() {
     this.primero = null;
-    this.n = 0;
+    this.n = null;
   }
 
   public isEmpty(): boolean {
     return (this.primero === null);
   }
+
+  public size(): number{
+    return this.n;
+  }
+
+  public add(item: Pixel, vertex:number){
+    let firstA: Nodo;
+    firstA = this.primero;
+    this.primero = new Nodo();
+    this.primero.item = item;
+    this.primero.vertex = vertex;
+    this.primero.next = firstA;
+    this.n++;
+
+  }
 }
 class Graph {
-  private pixel: number;
+  private vertex: number;
   private edge: number;
-  private adj: Bag[];
+  public adj: MyBag[];
 
-  constructor(pixel: number) {
-    if (pixel < 0) {
+  constructor(vertex: number) {
+    if (vertex < 0) {
       console.log("Numero Negativo");
     }
-    this.pixel = pixel;
+    this.vertex = vertex;
     this.edge = 0;
-    //   ady = (Bolsa<Integer>[]) new Bolsa[V];
-    for (var v = 0; v < pixel; v++) {
-     // ady[v] = new Bolsa<Integer>();
+    this.adj = new MyBag()[vertex];
+    for (var v = 0; v < vertex; v++) {
+      this.adj[v] = new MyBag();
     }
-    
   }
+   public V(): number{
+      return this.vertex;
+   }
+
+   public E(): number{
+     return this.edge;
+   }
+
+   public addEdge(v: number, w:number,vpix: Pixel, wpix:Pixel){
+     if(v<0 || v>=this.vertex)
+      console.log("Error");
+
+      if(w<0 || w >= this.vertex)
+      console.log("Error2");
+
+      this.edge++;
+      this.adj[v].add(wpix,w);
+      this.adj[w].add(vpix,v);
+
+   }
+}
+
+class NodoQ{
+    public item: Pixel;
+    public vertex: number;
+    public prox:NodoQ;
+}
+class Cola{
+  private n: number;
+  private inicio: NodoQ;
+  private ultimo: NodoQ;
+
+  constructor(){
+    this.inicio = null;
+    this.ultimo = null;
+    this.n = 0;
+  }
+
+  public isEmpty(): boolean{
+    return this.inicio == null;
+  }
+
+  public entrar(item: Pixel, vertex:number){
+    let anteriorUltimo: NodoQ;
+    anteriorUltimo = this.ultimo;
+    this.ultimo = new NodoQ();
+    this.ultimo.item = item;
+    this.ultimo.vertex = vertex;
+    this.ultimo.prox = null;
+    if(this.isEmpty)  this.inicio = this.ultimo;
+    else anteriorUltimo.prox = this.ultimo;
+    this.n++;
+  }
+
+  public salir(): number{
+    if(this.isEmpty) console.log("Cola vacia");
+    let item: number;
+    item = this.inicio.vertex;
+    this.inicio = this.inicio.prox;
+    this.n--;
+    if(this.isEmpty) this.ultimo = null;
+    return item;
+  }
+}
+class BFS{
+  private marcado: Boolean[];
+  private aristaHacia: number[];
+  
+  constructor(G: Graph, s:number, pixel: Pixel){
+    this.marcado = new Boolean[G.V()];
+    this.aristaHacia = new Number[G.V()];
+    this.bfs2(G,s,pixel);
+  }
+
+  private bfs2(G: Graph, s:number, pixel : Pixel){
+    let c = new Cola();
+    c.entrar(pixel,s);
+    
+    while(!c.isEmpty){
+        let v : number;
+
+
+        v = c.salir();
+
+        G.adj.forEach(element => {
+          
+        });
+        for ( key:NodoQ in G.adj[v]) {
+          if(!this.marcado[v]){
+            c.entrar(key.)
+          }
+        }
+        
+    }
+  }
+
 }
