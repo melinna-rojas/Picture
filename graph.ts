508267bdfbd391d081a431823ab1e1b4b8c12df0
Melinna Rojas Camargo <melin1504@gmail.com>
2019-06-25 14:41:26 -0500

    Adding gui

--- graph.ts
+++ graph.ts
@@ -13,11 +13,25 @@ class Bag {
     this.n = 0;
   }
 
-  public isEmpty():boolean{
-      return (this.primero === null);
+  public isEmpty(): boolean {
+    return (this.primero === null);
   }
 }
 class Graph {
   private pixel: number;
   private edge: number;
+  private adj: Bag[];
+
+  constructor(pixel: number) {
+    if (pixel < 0) {
+      console.log("Numero Negativo");
+    }
+    this.pixel = pixel;
+    this.edge = 0;
+    //   ady = (Bolsa<Integer>[]) new Bolsa[V];
+    for (var v = 0; v < pixel; v++) {
+     // ady[v] = new Bolsa<Integer>();
+    }
+    
+  }
 }
